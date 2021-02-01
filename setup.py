98e32ffc40c566550c64bd3ec243440013154303
Terrence Hung <terrencehung@hotmail.com>
2021-02-01 15:17:59 -0800

    Set up repo for publishing to PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="github-markdown-ui",
-    packages=setuptools.find_packages(),
+    name='github-markdown-ui',
+    version='0.0.1',
+    packages=setuptools.find_packages(exclude=['test']),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/github-markdown-ui/github-markdown-ui'
 )
